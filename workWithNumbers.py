382158aee70d4b3c09bc5ef5869db1ea068f6e66
devopsmaxit <git config --global user.email devopsmaxit@gmail.com>
2020-08-07 15:44:24 +0300

    adding main.py

---
+++ workWithNumbers.py
@@ -0,0 +1,29 @@
+import math
+# val
+esroni = 10.5
+shalem = 2
+shalem_sheni = 6
+print(shalem + shalem_sheni)
+print(shalem - shalem_sheni)
+print(shalem * shalem_sheni)
+print(shalem_sheni / shalem)
+# to the power of
+print(shalem_sheni ** shalem)
+
+x = 10 + 3 * 2 ** 2
+print(x)
+y = (10 + 3) * 2 ** 2
+print(y)
+
+# round the number
+print(round(esroni))  # if its  (x.5) it will round it up
+# absolut number
+print(abs(-2.6))
+
+# The object math (must import math in the begging of the script)
+print(math.ceil(2.4))
+print(math.factorial(6))
+# like the method round but always floor the number down
+print(math.floor(esroni))
+print(math.pi)
+
